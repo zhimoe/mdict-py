7e3010926fbe70167b6cbdfde4e352cea5f176eb
cod3fn <xiongwengui67@gmail.com>
2020-11-17 22:48:06 +0800

    增加Ctrl+L快捷键

--- static/index.js
+++ static/index.js
@@ -1,21 +1,31 @@
-$(document).ready(function ( e ) {
+$(document).ready(function (e) {
         $("#word").focus();
     }
 )
-$(document).keydown(function ( e ) {
+$(document).keydown(function (e) {
     if (e.keyCode === 13) {
         postQuery();
     }
 });
 
 //监听牛津8解释页面的外部单词链接
-$(document).on('click', 'a', function ( e ) {
+$(document).on('click', 'a', function (e) {
     console.log($(this).attr('href'));
     let href = $(this).attr('href');// "/cool"
     if (href.startsWith("/") && !href.startsWith("/#")) {
         $("#word").val(href.slice(1)) // "cool"
         postQuery();
         e.preventDefault()
+    }
+});
+
+// ctrl + L trigger input focus
+$(window).bind('keyup keydown', function (e) {
+    if ((e.ctrlKey || e.metaKey)
+        && String.fromCharCode(e.which).toUpperCase() === "L") {
+        e.preventDefault();
+        $("#word").val("");
+        $("#word").focus();
     }
 });
 
@@ -30,8 +40,9 @@ function postQuery() {
         type: "POST",
         data: {"word": word, "dict": dict},
         dataType: "html",
-        success: function ( data ) {
+        success: function (data) {
             $("#response").html(data);
         }
     });
 }
+
