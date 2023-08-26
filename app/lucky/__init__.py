5863830db3555f315073767c7b6012c164750df6
zhimoe <xiongwengui67@gmail.com>
2023-08-26 10:20:39 +0800

    update lucky word list

--- app/lucky/__init__.py
+++ app/lucky/__init__.py
@@ -1,10 +1,13 @@
-import random
 
 words_list = open('app/lucky/freq_words.txt').read().splitlines()
+
+count = -1
 
 
 def get_random_word() -> str:
     """
     试试手气, 随机抽取一个单词返回
     """
-    return random.choice(words_list)
+    global count
+    count += 1
+    return words_list[count % len(words_list)]
